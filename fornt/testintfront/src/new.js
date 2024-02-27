39c7c09a934da14b27720ec5f8fb0e472c5ffb71
kanishka udayanga <119073238+mskanishkaudayanga@users.noreply.github.com>
2024-02-27 13:49:20 +0530

    ka
    ka

---
+++ fornt/testintfront/src/new.js
@@ -0,0 +1,8 @@
+const New = () => {
+  return (
+    <div>
+      <h1>new</h1>
+    </div>
+  );
+};
+export default New;
--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.sendStatus(404);
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+app.post();
