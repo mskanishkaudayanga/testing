89f50a88302ce17ea2cbb0affbf08f0d7b0dc765
kanishka udayanga <119073238+mskanishkaudayanga@users.noreply.github.com>
2024-03-02 11:25:07 +0530

    mulin hari giye nathi eka

--- fornt/testintfront/src/App.js
+++ fornt/testintfront/src/App.js
@@ -1,47 +1,64 @@
+import React from "react";
 import "./App.css";
-import React, { useState } from "react";
-import "payhere-embed-sdk/dist/react.css";
-import Payhere from "payhere-embed-sdk/dist/react";
-import { Button, Flex } from "antd";
+// import PaymentModal from "./new";
 
 function App() {
+  const orderId = 45896588;
+  const name = "Just For You Mom Ribbon Cake";
+  const amount = 4500;
+
+  var payment = {
+    sandbox: true, // if the account is sandbox or real
+    merchant_id: "1226118", // Replace your Merchant ID
+    return_url: "http://sample.com/return",
+    cancel_url: "http://sample.com/cancel",
+    notify_url: "http://sample.com/notify",
+    order_id: orderId,
+    items: name,
+    amount: amount,
+    currency: "LKR",
+    first_name: "Saman",
+    last_name: "Perera",
+    email: "samanp@gmail.com",
+    phone: "0771234567",
+    address: "No.1, Galle Road",
+    city: "Colombo",
+    country: "Sri Lanka",
+    delivery_address: "No. 46, Galle road, Kalutara South", // optional field
+    delivery_city: "Kalutara", // optional field
+    delivery_country: "Sri Lanka", // optional field
+    custom_1: "", // optional field
+    custom_2: "", // optional field
+  };
+
+  // Called when user completed the payment. It can be a successful payment or failure
+  window.payhere.onCompleted = function onCompleted(orderId) {
+    console.log("Payment completed. OrderID:" + orderId);
+    //Note: validate the payment and show success or failure page to the customer
+  };
+
+  // Called when user closes the payment without completing
+  window.payhere.onDismissed = function onDismissed() {
+    //Note: Prompt user to pay again or show an error page
+    console.log("Payment dismissed");
+  };
+
+  // Called when error happens when initializing payment such as invalid parameters
+  window.payhere.onError = function onError(error) {
+    // Note: show an error page
+    console.log("Error:" + error);
+  };
+
+  function pay() {
+    window.payhere.startPayment(payment);
+  }
+
   return (
     <>
-      <SubscriptionPage />
+      <button onClick={pay}>Pay with Payhere</button>
+      wfwegf
     </>
   );
 }
 
-const SubscriptionPage = () => {
-  const [success, setSuccess] = useState(false);
-  const [showPayhere, setShowPayhere] = useState(false);
-
-  return (
-    <div id="payhere-modal">
-      <Button onClick={() => setShowPayhere(true)}>Continue to payment</Button>
-
-      <Payhere
-        selector="#payhere-modal"
-        embedURL={"https://app.payhere.co/altlabs/coffee"}
-        open={showPayhere}
-        onSuccess={(data) => {
-          console.log("Payhere success", data);
-          setSuccess(true);
-        }}
-        onFailure={(err) => {
-          console.log("Payhere failed", err);
-          setSuccess(true);
-        }}
-        onClose={() => {
-          setShowPayhere(false);
-          if (success) {
-            console.log("Payment success");
-          } else {
-            console.log("Payment failed");
-          }
-        }}
-      />
-    </div>
-  );
-};
 export default App;
