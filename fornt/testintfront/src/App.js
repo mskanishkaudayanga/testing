3a10a9f2a4ef59958e06d8cfe181e86f53602fb3
kanishka udayanga <119073238+mskanishkaudayanga@users.noreply.github.com>
2024-03-04 14:02:53 +0530

    d

--- fornt/testintfront/src/App.js
+++ fornt/testintfront/src/App.js
@@ -16,6 +16,7 @@ function App() {
     .toLocaleString("en-us", { minimumFractionDigits: 2 })
     .replaceAll(",", "");
   const currency = "LKR";
+
   const hash = md5(
     merchantId + orderId + amountFormated + currency + hashedSecret
   )
@@ -39,11 +40,6 @@ function App() {
     address: "No.1, Galle Road",
     city: "Colombo",
     country: "Sri Lanka",
-    delivery_address: "No. 46, Galle road, Kalutara South", // optional field
-    delivery_city: "Kalutara", // optional field
-    delivery_country: "Sri Lanka", // optional field
-    custom_1: "", // optional field
-    custom_2: "", // optional field
     hash: hash,
   };
 
